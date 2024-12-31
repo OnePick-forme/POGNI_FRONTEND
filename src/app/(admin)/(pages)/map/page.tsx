186a28f5da3b2fa0f6ba81da38120adafc2de60e
윤서 <Yunse070708@gmail.com>
2024-12-31 12:02:16 +0900

    feat : 오시는 글 작성 페이지 퍼블리싱

---
+++ src/app/(admin)/(pages)/map/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Postcode from "@actbase/react-daum-postcode";
+import Header from "@/app/components/header";
+import * as S from "./style.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface Address {
+  zonecode: number;
+  address: string;
+  buildingName?: string;
+  apartment?: string;
+}
+
+const Map = () => {
+  const router = useRouter();
+  const [isPostcodeOpen, setIsPostcodeOpen] = useState(true);
+
+  const getAddressData = (data: Address) => {
+    const selectedAddress =
+      data.address + (data.buildingName ? ` ${data.buildingName}` : "");
+    setIsPostcodeOpen(false);
+
+    router.push(
+      `/direction?selectedAddress=${encodeURIComponent(selectedAddress)}`
+    );
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={S.MapLayout}>
+        {isPostcodeOpen && (
+          <Postcode
+            style={{ flex: 1, width: "100%", height:"100vh", zIndex: 999 }}
+            jsOptions={{ animation: true }}
+            onSelected={(data: Address) => getAddressData(data)}
+            onError={(error: unknown) => {
+              console.error("Postcode error:", error);
+            }}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Map;
---
+++ src/app/(admin)/(pages)/map/style.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const MapLayout = style({
+  display: "flex",
+  width: "100%",
+  padding: "4% 10%",
+});
