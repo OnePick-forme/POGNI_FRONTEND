d6a905d2731d7a3f07c954959d7d43494615fc23
윤서 <Yunse070708@gmail.com>
2025-01-02 15:37:21 +0900

    feat : 게시글상세 페이지 퍼블리싱

---
+++ src/app/(admin)/(app)/detail/[id]/style.css.ts
@@ -0,0 +1,67 @@
+import { style } from "@vanilla-extract/css";
+import theme from "@/ui/style/theme.css";
+
+export const DetailLayout = style({
+  width: "100%",
+  padding: "0 10%",
+});
+
+export const DetailShow = style({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  padding: "0 60px 0 0",
+});
+
+export const DetailTitle = style({
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "59px 0 15px 0",
+  borderBottom: `2px solid ${theme.gray[200]}`,
+});
+
+export const DetailTitleText = style({
+  fontSize: "28px",
+  fontFamily: "Pretendard",
+  fontWeight: "400",
+  color: theme.gray[900],
+});
+
+export const DetailTitleDate = style({
+  fontSize: "24px",
+  fontFamily: "Pretendard",
+  fontWeight: "400",
+  color: theme.gray[500],
+});
+
+export const DetailContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  padding: "16px 0 0 0",
+});
+
+export const DetailTexts = style({
+  display: "flex",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  whiteSpace: "pre-wrap",
+  fontSize: "22px",
+  fontFamily: "Pretendard",
+  fontWeight: "500",
+  padding: "40px 0 42px 0",
+});
+
+export const DetailEdit = style({
+  display: "flex",
+  flexDirection: "row",
+  gap: "20px",
+  fontFamily: "pretendard",
+  fontWeight: "500",
+  fontSize: "20px",
+  color: theme.gray[500],
+});
+
+export const DetailImage = style({
+  padding: "0 0 328px 0",
+});
